b11abb78e27ef67a5300bed79cb8fba2c9a4bc15
Matheus Pedroni <mpedroni77@gmail.com>
2021-12-11 15:47:18 -0300

    Create nav/menu

---
+++ src/scripts/getActiveLink.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll('nav.menu ul li a');
+
+function getActiveLink(event) {
+  const hash = event.currentTarget.location.hash;
+
+  links.forEach(link => {
+    link.classList[link.hash === hash ? 'add' : 'remove']('active');
+
+    if(!hash) 
+      links.item('#about').classList.add('active');
+  })
+};
+
+window.addEventListener('hashchange', getActiveLink);
+window.addEventListener('load', getActiveLink);
