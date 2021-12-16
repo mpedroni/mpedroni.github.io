75db6009a51bc15255c2c25527dd0ae9c06d9c47
mpedroni <mpedroni77@gmail.com>
2021-12-16 02:22:36 +0000

    Deploying to gh-pages from @ mpedroni/mpedroni.github.io@c750cc1a24b78963932a95eef5f610bf1c537f5c 🚀

--- index.973ff5de.js
+++
@@ -1,2 +0,0 @@
-var links=document.querySelectorAll(".menu li a");function getActiveLink(e){var t=e.currentTarget.location.hash;links.forEach((function(e){e.classList[e.hash===t?"add":"remove"]("active"),t||links.item("#about").classList.add("active")}))}window.addEventListener("hashchange",getActiveLink),window.addEventListener("load",getActiveLink);
-//# sourceMappingURL=index.973ff5de.js.map
