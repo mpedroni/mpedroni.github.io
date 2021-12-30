2181b8e855d4912e7fd25e13fc45496a9ad50928
mpedroni <mpedroni77@gmail.com>
2021-12-30 02:57:21 +0000

    Deploying to gh-pages from @ mpedroni/mpedroni.github.io@b8f23db375a7c194cf8e51184255984e320a426c 🚀

---
+++ index.e9f60ca9.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll("article");function t(){var t=visualViewport.height+visualViewport.pageTop;e.forEach((function(e){e.offsetTop<t&&e.classList.add("card--animate")}))}window.addEventListener("scroll",t),window.addEventListener("load",t)}();
+//# sourceMappingURL=index.e9f60ca9.js.map
